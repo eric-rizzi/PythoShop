91fc390132163cbf03a78586fd35fff955305046
dwheadon <dwheadon@ucls.uchicago.edu>
2024-04-16 20:39:16 -0500

    Like the GUI, make the tests pass a dummy value for extra

--- tests/test_18_ext_draw_diagonals.py
+++ tests/test_18_ext_draw_diagonals.py
@@ -3,5 +3,5 @@ import testBase
 
 
 class Extension(testBase.TestBase, unittest.TestCase):
-    manip_func_name = "draw_diagonal_lines"
+    manip_func_name = "draw_sloping_lines"
     test_weight = 10
--- tests/test_19_ext_draw_x.py
+++ tests/test_19_ext_draw_x.py
@@ -3,5 +3,5 @@ import testBase
 
 
 class Extension(testBase.TestBase, unittest.TestCase):
-    manip_func_name = "draw_x"
+    manip_func_name = "draw_bisecting_diagonals"
     test_weight = 25
