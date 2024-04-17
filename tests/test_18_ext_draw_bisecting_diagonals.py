26f01cdfedf5de16671aa8ec7acc5d47942b5512
Eric Rizzi <eric.rizzi@gmail.com>
2024-04-17 08:14:45 -0500

    Rename test to match function

--- tests/test_18_ext_draw_bisecting_diagonals.py
+++ tests/test_18_ext_draw_bisecting_diagonals.py
@@ -1,7 +1,8 @@
 import unittest
+
 import tests.test_base as test_base
 
 
 class Extension(test_base.TestBase, unittest.TestCase):
-    manip_func_name = "draw_diagonal_lines"
+    manip_func_name = "draw_bisecting_diagonals"
     test_weight = 10
