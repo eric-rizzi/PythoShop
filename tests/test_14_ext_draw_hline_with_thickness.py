8fa3820c1cb6dd054936f49b261a99aa88c413fe
Eric Rizzi <eric.rizzi@gmail.com>
2024-04-20 13:43:30 -0500

    Bugfix: Include extra for thickness tests

--- tests/test_14_ext_draw_hline_with_thickness.py
+++ tests/test_14_ext_draw_hline_with_thickness.py
@@ -6,3 +6,7 @@ import tests.test_tool as test_tool
 class Extension(test_tool.Test, unittest.TestCase):
     manip_func_name = "draw_hline"
     test_weight = 5
+
+    def __init__(self, test):
+        super().__init__(test)
+        self.__class__.test_parameters.update({"extra": "4"})
