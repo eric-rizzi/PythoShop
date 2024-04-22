7a45a4849717b455d1233ef9be8dbff034d02710
dwheadon <dwheadon@ucls.uchicago.edu>
2024-04-21 23:13:05 -0500

    Add test for initial changes

--- tests/test_01_change_first_pixel.py
+++ tests/test_01_change_first_pixel.py
@@ -12,4 +12,8 @@ class Test(testTool.Test, unittest.TestCase):
         ["even"],
         ["bmpV1"],
     ]
-    test_weight = 50
+    test_weight = 25
+
+    def __init__(self, test):
+        super().__init__(test)
+        self.__class__.test_parameters.update({"clicked_coordinate": (0, 0), "color": (255, 0, 255)})
