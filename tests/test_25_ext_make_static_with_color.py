f11d5652c2b5ff69d3e556efe1e995d834ffc51a
Eric Rizzi <eric.rizzi@gmail.com>
2024-04-26 09:32:46 -0500

    Increase points for "make static"

--- tests/test_25_ext_make_static_with_color.py
+++ tests/test_25_ext_make_static_with_color.py
@@ -5,7 +5,7 @@ import tests.test_base as test_base
 
 class Extension(test_base.TestBase, unittest.TestCase):
     manip_func_name = "make_static"
-    test_weight = 10
+    test_weight = 15
 
     def __init__(self, test):
         super().__init__(test)
